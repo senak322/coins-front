429fdc188462ec42cdd4d3748452457d166f745d
Aleksandr Viznyi <senak98@yandex.ru>
2024-10-06 00:53:20 +0300

    fix api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,15 +1,12 @@
 import axios from "axios";
 // import { baseCurrencyUrl } from "./config";
 
-
-
 export async function getExchangeRates() {
   try {
-    const response = await axios.get('http://51.250.18.31/api/exchange-rate');
+    const response = await axios.get("/api/exchange-rate");
     return response.data;
   } catch (error) {
-    console.error('Error fetching exchange rates from backend:', error);
+    console.error("Error fetching exchange rates from backend:", error);
     return null;
   }
 }
-
