da880027db5e9d2f4d42ffb3e2b6e7b3ba4fec0e
Aleksandr Viznyi <senak98@yandex.ru>
2025-02-07 00:04:43 +0300

    fix

--- src/components/Auth/SignInModal.tsx
+++ src/components/Auth/SignInModal.tsx
@@ -47,10 +47,10 @@ export default function SignInModal({
 
     try {
       // API запрос на вход
-      const { userId, user, token, twoFA } = await login(loginInput, password);
+      const { user, token, twoFA } = await login(loginInput, password);
       if (twoFA) {
         // Сохраняем весь объект пользователя, а не только userId
-        setTempUser(user);
+        setTempUser(user._id);
         setIs2FADialogOpen(true);
       } else {
         // Если 2FA не требуется – сохраняем токен и обновляем состояние пользователя
