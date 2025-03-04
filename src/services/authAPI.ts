6c0b4c157aabbdf82bb8ace6dc1320729b61a3e5
Aleksandr Viznyi <senak98@yandex.ru>
2025-03-04 22:31:53 +0300

    fix

--- src/services/authAPI.ts
+++ src/services/authAPI.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/auth';
+const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api/auth';
 
 export const login = async (loginOrEmail: string, password: string) => {
   const response = await axios.post(`${API_URL}/login`, { loginOrEmail, password });
